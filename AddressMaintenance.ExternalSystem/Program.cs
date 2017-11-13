6d35943dc83dbcad37fb6c656005433f5143c788
Duncan Edwards <dun_edwards@yahoo.com>
2017-11-13 15:41:40 +0000

    Added external system project

---
+++ AddressMaintenance.ExternalSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using AddressMaintenance.Interfaces;
+using AddressMaintenance.Model;
+using System;
+using System.Configuration;
+using System.ServiceModel;
+
+namespace AddressMaintenance.ExternalSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+
+            //Create EndPoint address  
+            EndpointAddress endpointAddress = new EndpointAddress(ConfigurationManager.AppSettings["baseAddress"]);
+
+            //Pass Binding and EndPoint address to ChannelFactory  
+            var channelFactory = new ChannelFactory<IAddressMaintenanceService>(binding, endpointAddress);
+
+            //Now create the new channel as below  
+            IAddressMaintenanceService channel = channelFactory.CreateChannel();
+
+            CustomerPagedList customerPagedList;
+            int currentPage = 1;
+            //Call the service method on this channel as below
+            do
+            {
+                
+                customerPagedList = channel.GetAllCustomers(currentPage++);
+                foreach (var customer in customerPagedList.Customers)
+                {
+                    Console.WriteLine(customer.ToString() + "," + customer.CurrentAddress.ToString());
+                }
+            } while (customerPagedList.CurrentPage < customerPagedList.TotalPages);
+
+        }
+    }
+}
